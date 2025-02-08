db5fc758fd50f486dd67ca8ea258ba7085d77137
Almario Miano <almariomiano@gmail.com>
2025-02-08 13:30:57 +0800

    removed encryption

--- src/main/java/com/sparkcleancebu/zk9500_tray_app/Config.java
+++ src/main/java/com/sparkcleancebu/zk9500_tray_app/Config.java
@@ -4,17 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Base64;
 import java.util.Properties;
-
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
 public class Config {
 	private Properties properties = new Properties();
-	private Encryption encryption;
 	private String filePath;
 
 	public Config(String fileName) throws Exception {
@@ -50,23 +43,6 @@ public class Config {
 		try (FileInputStream fis = new FileInputStream(file)) {
 			properties.load(fis);
 		}
-
-		// initialize encryption service
-		this.encryption = new Encryption();
-
-		// retrieve the encryption key
-		String key = this.properties.getProperty(Encryption.ENCRYPTION_KEY_NAME, null);
-
-		if (key == null) {
-			// create fresh key
-			this.encryption.createKey();
-
-			// save it
-			this.properties.setProperty(Encryption.ENCRYPTION_KEY_NAME, this.encryption.getEncodedKey());
-			this.save();
-		} else {
-			this.encryption.setKey(key);
-		}
 	}
 
 	public String get(String key) throws Exception {
@@ -76,12 +52,12 @@ public class Config {
 			return null;
 		}
 
-		return this.encryption.decrypt(prop);
+		return prop;
 	}
 
 	public void set(String key, String value) throws Exception {
 		try {
-			this.properties.setProperty(key, this.encryption.encrypt(value));
+			this.properties.setProperty(key, value);
 		} catch (Exception e) {
 			throw new Exception("Unable to set property because " + key + " is " + value);
 		}
@@ -96,44 +72,5 @@ public class Config {
 	public void clear() throws IOException {
 		this.properties.clear();
 		this.save();
-	}
-
-	public static class Encryption {
-		public static final String ENCRYPTION_KEY_NAME = "CIPHER";
-		public static final String ENCRYPTION_ALGORITHM = "AES";
-		public SecretKey secretKey = null;
-
-		public void createKey() throws Exception {
-			KeyGenerator keyGen = KeyGenerator.getInstance(ENCRYPTION_ALGORITHM);
-			keyGen.init(128);
-			this.secretKey = keyGen.generateKey();
-		}
-
-		public String getEncodedKey() throws Exception {
-			if (this.secretKey == null) {
-				throw new Exception("Key is null");
-			}
-			return Base64.getEncoder().encodeToString(this.secretKey.getEncoded());
-		}
-
-		public void setKey(String encodedKey) throws Exception {
-			byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
-			this.secretKey = new SecretKeySpec(decodedKey, ENCRYPTION_ALGORITHM);
-		}
-
-		public String encrypt(String data) throws Exception {
-			Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
-			cipher.init(Cipher.ENCRYPT_MODE, this.secretKey);
-			byte[] encryptedBytes = cipher.doFinal(data.getBytes());
-			return Base64.getEncoder().encodeToString(encryptedBytes);
-		}
-
-		public String decrypt(String encryptedData) throws Exception {
-			Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
-			cipher.init(Cipher.DECRYPT_MODE, this.secretKey);
-			byte[] decodedBytes = Base64.getDecoder().decode(encryptedData);
-			byte[] decryptedBytes = cipher.doFinal(decodedBytes);
-			return new String(decryptedBytes);
-		}
 	}
 }
