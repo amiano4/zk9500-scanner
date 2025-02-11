35fa7f0ec7e9fe2c9d9482d2a78f385df94a3417
Almario Miano <almariomiano@gmail.com>
2025-02-11 18:48:27 +0800

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,14 +7,11 @@ module com.sparkcleancebu.zk9500_tray_app {
 	requires org.json;
 	requires jakarta.websocket.client;
 	requires java.net.http;
-	requires ZKFingerReader;
-	requires com.auth0.jwt;
 	requires javafx.base;
-	requires httpflux;
+	requires com.amiano4.httpflux;
+	requires ZKFingerReader;
 
 	opens com.sparkcleancebu.zk9500_tray_app to javafx.fxml;
-
-	opens com.sparkcleancebu.zk9500_tray_app.controllers to javafx.fxml;
 
 	exports com.sparkcleancebu.zk9500_tray_app;
 }
