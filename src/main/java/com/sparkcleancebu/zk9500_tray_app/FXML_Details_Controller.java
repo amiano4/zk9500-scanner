2ec4d97fa33d8ff1ea96549dc611eb654ee3f19a
Almario Miano <almariomiano@gmail.com>
2025-02-18 13:11:52 +0800

    updated details UI (removed unused elements)

--- src/main/java/com/sparkcleancebu/zk9500_tray_app/FXML_Details_Controller.java
+++ src/main/java/com/sparkcleancebu/zk9500_tray_app/FXML_Details_Controller.java
@@ -1,13 +1,9 @@
 package com.sparkcleancebu.zk9500_tray_app;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class FXML_Details_Controller {
-	@FXML
-	private Label debugging;
-
 	@FXML
 	private Label name;
 
@@ -24,17 +20,12 @@ public class FXML_Details_Controller {
 	private Label host;
 
 	@FXML
-	private Label lastScan;
-
-	@FXML
 	public void initialize() {
-		debugging.setText(null);
 		name.setText(null);
 		appId.setText(null);
 		branch.setText(null);
 		registeredAt.setText(null);
 		host.setText(null);
-		lastScan.setText(null);
 	}
 
 	public void display(Config config) throws Exception {
@@ -43,17 +34,5 @@ public class FXML_Details_Controller {
 		branch.setText(config.getBranch());
 		registeredAt.setText(config.getVerifiedAt());
 		host.setText(config.getHost());
-	}
-
-	public void debug(String content) {
-		Platform.runLater(() -> {
-			debugging.setText(content);
-		});
-	}
-
-	public void scan(String content) {
-		Platform.runLater(() -> {
-			lastScan.setText(content);
-		});
 	}
 }
